c9b79a18a3d219fc8393a120071a4e618263e686
Source Code <13261624778@163.com>
2021-02-05 21:58:55 +0800

    ok

--- pages/search/search.vue
+++ pages/search/search.vue
@@ -5,17 +5,17 @@
 			<view class="sys-head" :style="{ height: statusBarHeight }"></view>
 			<view class="serch-box">
 				<view class="serch-wrapper">
-					<input class="barInput" type="text" value="" v-model="searchValue" placeholder="输入搜索关键词" @input='handleInputChange'/>
+					<input class="barInput" type="text" value="" v-model="searchValue" placeholder="输入搜索关键词" @input='handleInputChange' @confirm='keyEnter(searchValue)'/>
 					<text class="iconfont icon-shanchu" @click="delValue"></text>
 				</view>
 			</view>
 		</view>
 
 		<!-- 内容展示 -->
-		<view class="contation">
+		<view class="contation" v-if="!keyList.length">
 
 			<!-- 历史搜索 -->
-			<view class="searchHis">
+			<view class="searchHis" v-show="isHis">
 				<!-- 标题 -->
 				<view class="titleHis">
 					<view class="iconfont icon-compass">
@@ -24,16 +24,7 @@
 				</view>
 				<!-- 内容 -->
 				<view class="contentHis">
-					<view class="movieHis">哈哈哈</view>
-					<view class="movieHis">哈哈</view>
-					<view class="movieHis">哈哈哈</view>
-					<view class="movieHis">哈哈哈哈哈哈哈哈哈</view>
-					<view class="movieHis">哈哈哈</view>
-					<view class="movieHis">哈哈哈</view>
-					<view class="movieHis">哈哈哈</view>
-					<view class="movieHis">哈哈</view>
-					<view class="movieHis">哈哈哈</view>
-					<view class="movieHis">哈哈哈哈哈哈哈哈哈</view>
+					<view class="movieHis" v-for="item in historyList" :key='item' @click="toInfo(item)">{{item}}</view>
 				</view>
 			</view>
 
@@ -47,12 +38,20 @@
 				</view>
 				<!-- 内容 -->
 				<view class="contentHot">
-					<view class="movieHot" v-for="item in hotList" :key='item.vod_id'>
+					<view class="movieHot" v-for="item in hotList" :key='item.vod_id' @click="toInfo(item.vod_name)">
 						{{item.vod_name}}
 					</view>
 				</view>
 			</view>
 
+		</view>
+		
+		
+		<!-- 搜索关键字内容列表 -->
+		<view class="keyList" v-else>
+			<view class="keyConent">
+				<view class="keyItem" @click="toInfo(item.vod_name)" v-for="item in keyList" :key='item.vod_id'>{{item.vod_name}}</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -70,10 +69,14 @@
 				searchValue: '', //搜索框查询值
 				keyList: [],  //搜索关键字列表
 				historyList: [], //搜索历史记录
+				isHis: false, //控制历史记录的显示
 			};
 		},
 		onLoad(options) {
+			//调用热门数据
 			this.getHotList()
+			//获取搜索历史记录
+			this.historyList = wx.getStorageSync('historyList') || []
 		},
 		methods:{
 			//发送请求获取热门搜索数据
@@ -81,8 +84,11 @@
 				const result = await request('/hotSearch')
 				this.hotList = result.list
 			},
+			
 			//获取input框输入内容
 			handleInputChange(event){
+				//显示历史记录
+				this.isHis = true
 				//获取输入框的信息
 				this.searchValue = event.detail.value.trim()
 				//判断是否节流
@@ -95,22 +101,66 @@
 				//函数节流
 				setTimeout( () => {
 					isSend = false
-				}, 300)
+				}, 500)
 			},
+			
 			//点击删除按钮的回调
 			delValue(){
 				//清空输入框
-				this.searchValue = ''
+				this.searchValue = '',
+				//清空索索关键字
+				this.keyList = []
 			},
+			
 			//发送请求获取关键字数据
 			async getKeyList(){
+				console.log(1)
 				
-				//发送请求获取数据,存入data
+				if(!this.searchValue){
+					this.keyList = []
+					return
+				}
+				console.log(2)
+				// // 发送请求获取数据,存入data
 				const result = await request('/search?', {wd: this.searchValue})
 				this.keyList = result.list 
+				// console.log(this.historyList)
 				
+				// this.historyList.unshift(this.searchValue)
+				// // console.log(this.historyList)
+				
+				// //存入本地
+			// 	wx.setStorageSync('historyList', this.historyList)
+			},
+			
+			//点击跳转达详情页面
+			toInfo(keyValue){
+				console.log(keyValue)
+				//跳转页面传递关键字数据到详情页
+				wx.navigateTo({
+					url: '/pages/searchInfo/searchInfo?keyValue=' + keyValue
+				})
+				//点击跳转的关键字存入历史记录
+				let his = this.historyList
+				//判断关键字历史是否重复
+				if(his.indexOf(keyValue) !== -1){
+				   his.splice(his.indexOf(keyValue), 1)
+				}
+				//存入历史记录
+				his.unshift(keyValue)
+				this.historyList = his
+				wx.setStorageSync('historyList', this.historyList)
+			},
+			
+			// 回车事件
+			keyEnter(keyValue){
+				console.log(23333)
+				console.log('2',keyValue)
+				wx.navigateTo({
+						url: '/pages/searchInfo/searchInfo?keyValue=' + keyValue
+				})
+				//存入历史记录
 				this.historyList.unshift(this.searchValue)
-				//存入本地
 				wx.setStorageSync('historyList', this.historyList)
 			}
 		}
@@ -245,6 +295,23 @@
 				justify-content: flex-start;
 				margin-right: 18rpx;
 				white-space: nowrap;
+				}
+			}
+		}
+		
+		
+		.keyList {
+			background: #fff;
+			border-radius: 30rpx;
+			height: 100%;
+			padding: 20rpx;
+			.keyConent {
+				margin-top: 50rpx;
+				.keyItem{
+					border-bottom: 1rpx solid #333333;
+					font-size: 27rpx;
+					padding: 25rpx 0;
+					color: #8c8c8c;
 				}
 			}
 		}
