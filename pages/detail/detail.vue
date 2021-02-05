07d68c7ce783cedf3d85ec773cf21ef0da16b6f8
xiaoxiao <2912598713@qq.com>
2021-02-05 20:08:27 +0800

    '详情页完成'

--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -39,7 +39,8 @@
 				<text class="douban">豆瓣:{{videoInfo.vod_score}}</text>
 
 				<text class="iconfont icon-shixinxingxing" v-for="(item,index) in xinxinObj.wholeNum" :key="index"></text>
-				<image class="halfStar" src="/static/images/detail/half.svg" mode=""></image>
+					<image class="halfStar" src="/static/images/detail/half.svg" mode="" 
+					 v-if="xinxinObj.isHalf"></image>
 				<text class="iconfont icon-shixinxingxing1" v-for="(item,index) in xinxinObj.whiteNum" :key="index"></text>
 			</view>
 			<view class="play" @click="toPlay">
