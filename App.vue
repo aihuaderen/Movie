805b7d7193cb685854b3a2ea095e3e33e913aec6
techoo <techoo@foxmail.com>
2021-02-06 10:37:45 +0800

    弹幕

--- App.vue
+++ App.vue
@@ -2,6 +2,45 @@
 <script>
 	export default {
 		onLaunch: function() {
+			//弹幕存储
+			if(!uni.getStorageSync('danmuList') || uni.getStorageSync('danmuList').length === 0) {
+				uni.setStorageSync('danmuList', [{
+					text: '一大波弹幕即将来袭',
+					color: '#ff0000',
+					time: 1
+				},
+				{
+					text: '发条弹幕见证当下',
+					color: '#00ff00',
+					time: 2
+				},
+				{
+					text: 'Oh,MyGod!这也太厉害了吧',
+					color: '#0000ff',
+					time: 3
+				},
+				{
+					text: '爱了,爱了',
+					color: '#fff',
+					time: 3
+				}])
+			}
+			//时间格式化
+			Date.prototype.Format = function (fmt) {
+				var o = {
+					"M+": this.getMonth() + 1, //月份 
+					"d+": this.getDate(), //日 
+					"H+": this.getHours(), //小时 
+					"m+": this.getMinutes(), //分 
+					"s+": this.getSeconds(), //秒 
+					"q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+					"S": this.getMilliseconds() //毫秒 
+				};
+				if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+				for (var k in o)
+				if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+				return fmt;
+			}
 			console.log('App Launch')
 		},
 		onShow: function() {
